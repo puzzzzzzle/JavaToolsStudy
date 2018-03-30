8f6d155c5f98aa79ed987665b191547c2fe1a254
puzzzzzzle <2359173906@qq.com>
2018-03-30 15:24:13 +0800

    Stream Study

---
+++ src/main/java/group/zhangtao/grammer/MBigNumber.java
@@ -0,0 +1,22 @@
+package group.zhangtao.grammer;
+
+import java.math.BigInteger;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class MBigNumber {
+    public static void main(String[] args) {
+        System.out.println("1\n1");
+        Stream.generate(new Supplier<BigInteger>() {
+            BigInteger i =new BigInteger("1");
+            BigInteger j= new BigInteger("1");
+            @Override
+            public BigInteger get() {
+                BigInteger temp = i.add(j);
+                i = j;
+                j = temp;
+                return j;
+            }
+        }).forEach(i-> System.out.println(i.toString()));
+    }
+}
